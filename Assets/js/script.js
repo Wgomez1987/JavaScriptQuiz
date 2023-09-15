6fa44a618b31a92f5c77dfcc7b7dc06916a118c6
Wgomez1987 <gomez.william.a1@gmail.com>
2023-09-15 16:59:32 -0700

    Beginning JavaScript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,25 @@
 
+ //get timer to work
+ var timeLeft = 30
+ var elem = document.getElementById('countdown-timer');
+ var timerId = setInterval(countdown, 1000);
+ function countdown() {
+    if (elem == -1) {
+      clearTimeout(timerId);
+      doSomething();
+    } else {
+      elem.innerHTML = timeLeft + ' seconds remaining';
+      timeLeft--;
+    }
+  }
+
+  
+  
+
+ //make questions work, and switch pages
+
+ //ensure timer loses time with every wrong answer
+
+ //get this shit to work.
+
+ 
